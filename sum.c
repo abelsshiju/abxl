6c1e2b37b3edc80a36570772a2079f50860b66b4
Your SarayuSuresh18 <sarayusuresh18@gmail.com>
2025-04-03 12:08:29 +0530

    2nd commit

---
+++ sum.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+void main()
+{
+int a ,b ,sum;
+printf("enter the number") ;
+scanf("%d",&a) ;
+printf("enter the number") ;
+scanf("%d",&b) ;
+sum=a+b;
+printf("sum=%d\n",sum);
+}
+  
